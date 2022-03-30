4cacf3bf6f1064d05e7a7a5be51c91851fb4212d
Stephen Shanko <scubashanko@gmail.com>
2022-03-30 14:02:25 -0400

    Create environment sample and readme

---
+++ complete/environment-sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export POLARIS_BASE_URL="my polaris url"
+export CODEDX_BASE_URL="my code dx url"
+export POLARIS_PAT_TOKEN="my polaris personal access token"
+export CODEDX_PAT_TOKEN="my code dx personal access token"
+export POLARIS_APPLICATION_ID="my polaris application"
+export CODEDX_PROJECT_ID="my code dx parent project"
